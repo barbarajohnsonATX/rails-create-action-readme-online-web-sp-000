982fda68382f237812b4b2b731956fb53a832de7
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-30 03:57:34 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,7 @@ class PostsController < ApplicationController
   def create 
     post = Post.new
     post.title = params[:title]
-    post.description = params[:description])
+    post.description = params[:description]
     post.save
   end 
   
